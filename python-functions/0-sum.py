1c86198939982f2d1e42de2f644e0aa7d93130c1
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-23 19:23:42 +0100

    Update 0-sum.py

--- python-functions/0-sum.py
+++ python-functions/0-sum.py
@@ -1,8 +1,8 @@
 def add(a, b):
     return a + b
 
-print(add(1, 2))
-print(add(98, 0))
-print(add(100, -2))
-print(add(-100, -2))
-print(add(0, 0))
+print(add(1, 2)) # Correct output: 3
+print(add(98, 0)) # Correct output: 98
+print(add(100, -2)) # Correct output: 98
+print(add(-100, -2)) # Correct output: -102
+print(add(0, 0)) # Correct output: 0
