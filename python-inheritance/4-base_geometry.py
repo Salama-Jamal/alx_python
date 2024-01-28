f24d92169a4b7855ea92a06319dda8b6dfd45767
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-28 02:13:16 +0100

    Update 4-base_geometry.py

--- python-inheritance/4-base_geometry.py
+++ python-inheritance/4-base_geometry.py
@@ -1,11 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-=======
-# 4-base-geometry.py
-# dricko147
 """Defines a base geometry class BaseGeometry."""
 
->>>>>>> 02eecf01c6803a7690f4e1683e7d679fffd87cd5
 
 class BaseGeometry:
     """Represent base geometry."""
@@ -13,16 +8,3 @@ class BaseGeometry:
     def area(self):
         """Not implemented."""
         raise Exception("area() is not implemented")
-<<<<<<< HEAD
-
-BaseGeometry = __import__('4-base_geometry').BaseGeometry
-
-bg = BaseGeometry()
-print(dir(bg))
-
-
-
-
-
-=======
->>>>>>> 02eecf01c6803a7690f4e1683e7d679fffd87cd5
