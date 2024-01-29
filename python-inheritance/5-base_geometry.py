02bfa42441ae051a8222ec9a7780b822562342ad
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-29 23:04:24 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,19 +1,31 @@
-'''
-The below program will be
-telling more about inhertitance
-in python programming
-'''
-class BaseGeometry:
-    '''
-    The below program will be
-    telling more about inhertitance
-    in python programming
-    '''  
-    def area(self):
-        raise Exception("area() is not implemented")
+#!/usr/bin/python3
+"""Defines a class BaseGeometry based on 4-base_geometry.py"""
 
+
+class BaseGeometry:
+    """Class BaseGeometry.
+    """
+    def area(self):
+        """Area function.
+
+        Raises:
+            Exception: if area is not implemented.
+        """
+        raise Exception("area() is not implemented")
+    
     def integer_validator(self, name, value):
+        """Validate the input value.
+
+        Args:
+            name (str): The name of the value being validated.
+            value (int): The value to be validated.
+
+        Raises:
+            TypeError: If the value is not an integer.
+            ValueError: If the value is less than or equal to 0.
+
+        """
         if not isinstance(value, int):
-            raise TypeError(f"{name} must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError(f"{name} must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
