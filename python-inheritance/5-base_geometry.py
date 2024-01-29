9e00209d2aceb0efad42e1318ac56f5ff9595928
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-29 22:19:10 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,7 +1,19 @@
-#!/usr/bin/python3
-"""Defines an empty class BaseGeometry."""
-
+"""
+This module contains the definition of the BaseGeometry class.
+"""
 
 class BaseGeometry:
-    """Represent base geometry."""
-    pass
+    """
+    This is an empty class that serves as a base for geometry-related classes.
+    """
+    def area(self):
+        """
+        This is a method that raises an exception
+        """
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        
+        if not isinstance(value, int):
+            raise TypeError(f"{name} must be an integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than 0")
