cbd57752e04bb83edec7622d1bcebef94b10779f
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-29 22:16:55 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,21 +1,7 @@
-"""
-This base geometry class program have a validator
-and it cares the self, name and value
-"""
+#!/usr/bin/python3
+"""Defines an empty class BaseGeometry."""
+
 
 class BaseGeometry:
-    """ This is the class for Base Geometry"""
-    def __init__(self):
-        """ This function is for initialization """ 
-        pass 
-    def area(self):
-        """ This method is for the area of the base geometry with
-            an exception
-        """    
-        raise Exception("area() is not implemented")
-    def integer_validator(self, name, value):
-        """ This is the integer validator of the method"""
-        if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
-        elif value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+    """Represent base geometry."""
+    pass
