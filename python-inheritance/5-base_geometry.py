b0c44c9eb41348866f58827ac18ea20416a4ba3b
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-01-29 23:03:02 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,22 +1,19 @@
-"""
-a python file for question no. 5
-"""
-class BaseGeometry():
-    """
-    a class
-    """
+'''
+The below program will be
+telling more about inhertitance
+in python programming
+'''
+class BaseGeometry:
+    '''
+    The below program will be
+    telling more about inhertitance
+    in python programming
+    '''  
     def area(self):
-        """
-        Public instance method, that raises an Exception with the message: area() is not implemented
-        """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """
-        Public instance method, that validates value.
-        you can assume name is always a string
-        """
         if not isinstance(value, int):
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError(f"{name} must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError(f"{name} must be greater than 0")
