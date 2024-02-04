f9451e14a182af6c743d599e57c9e4946b155148
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-02-04 23:14:47 +0100

    Update 5-base_geometry.py

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,6 +1,10 @@
 """
 This module contains the definition of the BaseGeometry class.
 """
+BaseGeometry = __import__('5-base_geometry').BaseGeometry
+
+bg = BaseGeometry()
+print(dir(bg))
 
 class BaseGeometry:
     """
