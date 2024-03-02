d8cce2f44586a81d387e7058aa47def78931d4de
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-03-02 21:11:31 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,26 +1,30 @@
 import csv
-import requests 
-from sys import argv
+import requests
+import sys
 
-id = argv[1]
-url1 = f'https://jsonplaceholder.typicode.com/users/{id}/todos'
-empurl= f'https://jsonplaceholder.typicode.com/users/{id}'
 
-res1 = requests.get(url1)
-data1 = res1.json()
+url = "https://jsonplaceholder.typicode.com/users"
+user_id = sys.argv[1]
+endpoint1 = f"{url}/{user_id}"
+endpoint2 = f"{url}/{user_id}/todos"
 
-res2 = requests.get(empurl)
-employeedata = res2.json()
+response1 = requests.get(url=endpoint1)
+response2 = requests.get(url=endpoint2)
 
-USER_ID = employeedata['id']
-USERNAME = employeedata['username']
-TASK_COMPLETED_STATUS = ''
-TOTAL_NUMBER_OF_TASKS = len(data1)
-TASK_TITLE = ''
+data1 = response1.json()
+data2 = response2.json()
+myarray = []
+csvheader = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
 
-with open(f'{USER_ID}.csv', 'w', newline='') as file:
-    writer = csv.writer(file)
-    for i in range(len(data1)):
-        TASK_COMPLETED_STATUS = data1[i]['completed']
-        TASK_TITLE = data1[i]['title']
-        writer.writerow([USER_ID,USERNAME,TASK_COMPLETED_STATUS,TASK_TITLE])
+for task in data2:  # Iterate over tasks from the second endpoint
+    listing = [task['userId'], data1['username'], task['completed'], task['title']]
+    myarray.append(listing)
+
+
+with open(f"{user_id}.csv", 'w', encoding='UTF8', newline='') as f:
+    writer = csv.writer(f)
+
+    writer.writerow(csvheader)
+    writer.writerows(myarray)
+
+
