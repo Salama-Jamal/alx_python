80372a70e918febcdb8b94d7a184f7c02eeeeb9c
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-03-02 21:00:14 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,32 +1,40 @@
-"""import libraries alphabetically"""
+"""
+Check student .CSV output of user information
+"""
+
 import csv
 import requests
 import sys
 
-"""Create a function to retrieve data"""
-def getData(id):
-    """
-    Data from json API
-    """
-    usersurl = "https://jsonplaceholder.typicode.com/users/{}".format(id)
-    todourl = "{}/todos".format(usersurl)
+users_url = "https://jsonplaceholder.typicode.com/users?id="
+todos_url = "https://jsonplaceholder.typicode.com/todos"
 
-    request1 = requests.get(usersurl)
-    result = request1.json()
-    userid = result['id']
-    username = result['username']
 
-    request2 = requests.get(todourl)
-    tasks = request2.json()
+def user_info(id):
+    """ Check user information """
 
-    with open("{}.csv".format(userid), "w", newline='') as csvfile:
-        writer = csv.writer(csvfile, quoting = csv.QUOTE_ALL)
-        for task in tasks:
-            writer.writerow([userid, username, task['completed'], task['title']])
+    # Calculate total number of tasks from API
+    total_tasks = sum(1 for task in requests.get(todos_url).json() if task['userId'] == id)
+
+    # Count number of tasks in CSV file
+    num_tasks_in_csv = 0
+    with open(str(id) + ".csv", 'r') as f:
+        reader = csv.reader(f)
+        # Skip header row
+        next(reader)
+        for _ in reader:
+            num_tasks_in_csv += 1
+
+    # Compare total tasks with tasks in CSV
+    if total_tasks == num_tasks_in_csv:
+        print("Number of tasks in CSV: OK")
+    else:
+        print("Number of tasks in CSV: Incorrect")
+
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        id = sys.argv[1]
-    else:
-        id = 1
-    getData(str(id))
+    if len(sys.argv) != 2:
+        print("Usage: python3 script_name.py <employee_id>")
+        sys.exit(1)
+
+    user_info(int(sys.argv[1]))
