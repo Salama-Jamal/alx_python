e9b38a39278936b2c19a43fb0e29c9a96c53a669
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-03-02 21:07:15 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,43 +1,22 @@
 import csv
 import requests
 import sys
-
-def employee_info(employee_id):
-    """Retrieve employee details and tasks from the given URL."""
-    # Construct URLs for employee details and tasks
+def get_employee_info(employee_id):
+    """Fetch the employee details from the given url by appending the employee_id and convert the data to json"""
     employee_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
-    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
-
-    # Make HTTP GET requests to fetch data
     employee_response = requests.get(employee_url)
-    todos_response = requests.get(todos_url)
-
-    # Extract relevant information from the responses
     employee_data = employee_response.json()
+    """fetch the employee's todo by appending the todo route to the url"""
+    todos_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
+    todos_response = requests.get(todos_url)
     todos_data = todos_response.json()
-
-    # Initialize a list to store task details
-    tasks = []
-
-    # Extract task details
-    for task in todos_data:
-        task_details = (
-            employee_id,
-            employee_data['username'],
-            task['completed'],
-            task['title']
-        )
-        tasks.append(task_details)
-
-    """creating csv file for the empoyee"""
+    """create csv file for the empoyee"""
     file_name = f"{employee_id}.csv"
-    with open(file_name, "w", newline='') as csv_file:
+    with open(file_name, mode="w", newline='') as csv_file:
         fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE' ]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
         """Write the csv headers"""
         writer.writeheader()
-
         """Write the rows under the epecified columns"""
         for task in todos_data:
             writer.writerow({
@@ -46,15 +25,12 @@ def employee_info(employee_id):
                 'TASK_COMPLETED_STATUS': task['completed'],
                 'TASK_TITLE': task['title']
             })
-
-
+"""Obtain the employees details from the command line using the sys module"""
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python script_name.py employee_id.")
         sys.exit(1)
-
-    # Extract employee ID from command-line argument
+    """Get the employee id from the second argument"""
     employee_id = int(sys.argv[1])
-
-    # Call the function to retrieve and record employee task details
-    employee_info(employee_id)
+    """Call the function with the provided id"""
+    get_employee_info(employee_id)
