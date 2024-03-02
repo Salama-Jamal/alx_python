20a8ce231ead83c54c37a51fe7638f689c5a6da4
Jamal Salama <148999243+Salama-Jamal@users.noreply.github.com>
2024-03-02 21:01:22 +0100

    Update 1-export_to_CSV.py

--- api/1-export_to_CSV.py
+++ api/1-export_to_CSV.py
@@ -1,40 +1,58 @@
-"""
-Check student .CSV output of user information
-"""
+#!/usr/bin/python3 
 
+"""
+this is a python file that export it's data to csv"""
 import csv
-import requests
-import sys
+import requests 
+import sys 
 
-users_url = "https://jsonplaceholder.typicode.com/users?id="
-todos_url = "https://jsonplaceholder.typicode.com/todos"
+def get_employee_tasks(user_id):
+    """Get the endpoint"""
+    employee_url = "https://jsonplaceholder.typicode.com/users/{}"
+    employee_response = requests.get(employee_url.format(user_id))
+     
+    #  if status code is correct get employee details
+    if employee_response.status_code == 200:
+        employee_details = employee_response.json()
+        user_id = employee_details['id']
+        username = employee_details['username']
+    else: 
+        print(f"Failed to retrieve employee's details:{employee_response.status_code}")
+        return None
+    
+    
+    todo_url = f"https://jsonplaceholder.typicode.com/users/{user_id}/todos"
+    todo_response = requests.get(todo_url)
+    
 
-
-def user_info(id):
-    """ Check user information """
-
-    # Calculate total number of tasks from API
-    total_tasks = sum(1 for task in requests.get(todos_url).json() if task['userId'] == id)
-
-    # Count number of tasks in CSV file
-    num_tasks_in_csv = 0
-    with open(str(id) + ".csv", 'r') as f:
-        reader = csv.reader(f)
-        # Skip header row
-        next(reader)
-        for _ in reader:
-            num_tasks_in_csv += 1
-
-    # Compare total tasks with tasks in CSV
-    if total_tasks == num_tasks_in_csv:
-        print("Number of tasks in CSV: OK")
+    if todo_response.status_code == 200:
+        task_data = todo_response.json()
     else:
-        print("Number of tasks in CSV: Incorrect")
+        print(f"Failed to retrieve employee's todos:{employee_response.status_code}")
+        return None
 
+    csv_filename = f"{user_id}.csv"
+    with open(str(user_id) + ".csv", 'w', newline="") as f:
+
+
+         fieldnames = ['user_id', 'username', 'task_completed_status', 'task_title']
+         writer = csv.DictWriter(f, fieldnames=fieldnames)
+        
+         row = [{
+                 'user_id': user_id,
+                 'username': username,
+                 'task_completed_status': task['completed'],
+                 'task_title': task['title']
+                 } for task in task_data]
+         writer.writerows(row)
+                 
+             
+    print(f"The csv_file{csv_filename} was successfully created with data from {user_id}")
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python3 script_name.py <employee_id>")
-        sys.exit(1)
-
-    user_info(int(sys.argv[1]))
+    if len(sys.argv) > 1:
+         user_id = int(sys.argv[1])
+    else:
+      user_id = 1
+      get_employee_tasks(user_id)
+         
